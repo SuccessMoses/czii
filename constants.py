093e00c209872fc4a3cd5ded008447b96686f4c1
Success Moses <syko2021@gmail.com>
2025-01-30 21:50:46 +0100

    Update constants.py

--- constants.py
+++ constants.py
@@ -17,7 +17,6 @@ from monai.transforms import (
 )
 
 import cc3d
-from helper import extract_3d_patches_minimal_overlap, reconstruct_array, dict_to_df
 import pandas as pd
 from callback import compute_lb
 
@@ -138,72 +137,3 @@ with open(ROOT_FILE) as f:
 
 
 ROOT = copick.from_file(ROOT_FILE)
-
-INFERENCE_TRNASFORMS = Compose([
-    EnsureChannelFirstd(keys=["image"], channel_dim="no_channel"),
-    NormalizeIntensityd(keys="image"),
-    Orientationd(keys=["image"], axcodes="RAS")
-])
-
-id_to_name = {1: "apo-ferritin", 
-              2: "beta-amylase",
-              3: "beta-galactosidase", 
-              4: "ribosome", 
-              5: "thyroglobulin", 
-              6: "virus-like-particle"}
-
-BLOB_THRESHOLD = 500
-CERTAINTY_THRESHOLD = 0.5
-
-CLASSES = [1, 2, 3, 4, 5, 6]
-
-def validation(model, valid_id):
-    with torch.no_grad():
-        location_df = []
-        for run in ROOT.runs:
-            print(run)
-
-            tomo = run.get_voxel_spacing(10)
-            tomo = tomo.get_tomogram(TOMOGRAM_ALGORITHM).numpy()
-
-            tomo_patches, coordinates  = extract_3d_patches_minimal_overlap([tomo], 96)
-
-            tomo_patched_data = [{"image": img} for img in tomo_patches]
-
-            tomo_ds = CacheDataset(data=tomo_patched_data, transform=INFERENCE_TRNASFORMS, cache_rate=1.0)
-
-            pred_masks = []
-
-            for i in range(len(tomo_ds)):
-                input_tensor = tomo_ds[i]['image'].unsqueeze(0).to("cuda")
-                model_output = model(input_tensor)
-
-                probs = torch.softmax(model_output[0], dim=0)
-                thresh_probs = probs > CERTAINTY_THRESHOLD
-                _, max_classes = thresh_probs.max(dim=0)
-
-                pred_masks.append(max_classes.cpu().numpy())
-                
-
-            reconstructed_mask = reconstruct_array(pred_masks, coordinates, tomo.shape)
-            
-            location = {}
-
-            for c in CLASSES:
-                cc = cc3d.connected_components(reconstructed_mask == c)
-                stats = cc3d.statistics(cc)
-                zyx=stats['centroids'][1:]*10.012444 #https://www.kaggle.com/competitions/czii-cryo-et-object-identification/discussion/544895#3040071
-                zyx_large = zyx[stats['voxel_counts'][1:] > BLOB_THRESHOLD]
-                xyz =np.ascontiguousarray(zyx_large[:,::-1])
-
-                location[id_to_name[c]] = xyz
-
-
-            df = dict_to_df(location, run.name)
-            location_df.append(df)
-        
-        location_df = pd.concat(location_df)
-        location_df.insert(loc=0, column='id', value=np.arange(len(location_df)))
-
-    _, score = compute_lb(location_df, valid_id)
-    return score
